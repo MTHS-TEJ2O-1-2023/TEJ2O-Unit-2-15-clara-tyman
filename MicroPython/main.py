a5bb90a0dcf73e5d03048ca2820405c6f9ccf02d
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-15 18:28:13 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,37 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Clara
+Created on: Nov 2023
+This module is a Micro:bit MicroPython program moves the LED's on the Microbit around the edge of the screen. 
 """
 
 from microbit import *
 
+# variables
+set_pixel = None
+loopCounter = 0
+led_coordinate = 0
 
-display.scroll("Hello, World!")
+# Setup
+display.clear()
+display.show(Image.SILLY)
+sleep(500)
+
+# Press button A
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        loopCounter = 0
+
+        while loopCounter < 5:
+            led_coordinate = 0
+            while led_coordinate <= 3:
+                led_coordinate = 0
+                while led_coordinate <= 4:
+                    sleep(500)
+                    display.set_pixel(led_coordinate, loopCounter, 9)
+                    led_coordinate += 1
+
+                    led_coordinate = 0
+                    loopCounter += 1
+                display.clear()
+                sleep(500)
