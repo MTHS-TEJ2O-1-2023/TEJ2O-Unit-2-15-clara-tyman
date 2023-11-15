7659abc4f8aeb21690d7929f97cf2c67a50f65db
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-15 12:50:28 -0500

    ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara
+ * Created on: Nov 2023
+* This program moves a sprite around the edge of the microbit.
 */
 
-basic.showString('Hello, World!')
+// variables
+let xValue: number = 0
+let loopCounter: number = 0
+let sprite: game.LedSprite = null
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Silly)
+
+// when button A is pressed, move sprite around edge
+input.onButtonPressed(Button.A, function () {
+
+  // prep screen
+  basic.clearScreen()
+  sprite = game.createSprite(0, 0)
+
+  loopCounter = 0
+  while (loopCounter <= 3) {
+
+    xValue = 0
+    while (xValue <= 4) {
+      basic.pause(500)
+      sprite.move(1)
+      xValue++
+    }
+    sprite.turn(Direction.Right, 90)
+    loopCounter++
+  }
+
+  sprite.delete()
+  basic.pause(500)
+  basic.showIcon(IconNames.Silly)
+})
